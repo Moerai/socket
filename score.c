7436264af3d4cd6c5a2261229c9b9c197553d349
KrToTo <friendnt@naver.com>
2018-09-11 10:43:16 +0900

    동적 매모리 할당으로 변경

--- score.c
+++ score.c
@@ -1,23 +1,37 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-//점수를 입력하면 학점을 출력해주는 프로그램
-char get_grade(int score){
-	if(score >= 90)
-		return 'A';
+//점수를 입력하면 학점을 출력해주는 프로그램2
+void get_grade(int score, char *grade){
+	if(score >= 95)
+		strcpy(grade, "A+");
+	else if (score >= 90)
+		strcpy(grade, "A0");
+	else if (score >= 85)
+		strcpy(grade, "B+");
 	else if (score >= 80)
-		return 'B';
-	else if (score >= 70)
-		return 'C';
+		strcpy(grade, "B0");
+	else if (score >= 75)
+		strcpy(grade, "C+");
+	else if (scroe >= 60)
+		strcpy(grade, "C0");
 	else
 		return 'F';
 }
 
-int main(){
+int main(int argc, char *argv[]){
 	int score = 80;
-	scanf("%d", &score);
-	char grade;
+	//실행시 점수를 함께 입력 받기:
+	if(argc <2 ){
+		printf("[Error]실행 시 점수를 함께 입력하시오\n");
+		return 1;
+	}
+	score = atoi(argv[1]);
+	char *grade = malloc(sizeof(char) * 10);
 
-	grade = get_grade(score);
-	printf("당신의 학점은 %c입니다. \n", grade);
+	get_grade(score, grade);
+	printf("당신의 학점은 %s 입니다. \n", grade);
+	free(grade);
 	return 0;
 }
